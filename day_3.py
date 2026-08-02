520e4bd24ff741e285547c847f2ff8f7c9d000f4
himanshusapkota <himanshuspkt@gmail.com>
2026-08-02 14:04:36 +0545

    3

---
+++ day_3.py
@@ -0,0 +1,25 @@
+#String Slicing
+name="Himanshu"
+print(name[0:5])
+
+#How to find the length of the String? 
+
+# We can find it by using the length function
+ 
+ #Example Code
+ 
+     
+a="Himanshu"
+print(len(a))
+
+fruit="apple"
+len1=len(fruit)
+
+print("Apple is" , len1 , "letter word.")
+
+
+
+a="GOOONING"
+print(a[1:4])
+
+
