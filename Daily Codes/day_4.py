bc08664802300b126b17db1cc0dcd0c55f5dd645
himanshusapkota <himanshuspkt@gmail.com>
2026-08-03 12:31:32 +0545

    Code

---
+++ Daily Codes/day_4.py
@@ -0,0 +1,67 @@
+
+#Skipped If else because I already Know it
+
+
+#Match Case Statement 
+ #Example Program
+ 
+day=int(input("Enter a number : "))
+
+match day:
+    case 1:
+        print("Sunday")
+        
+    case 2:
+        print("Monday")   
+    
+    case 3:
+        print("Tuesday") 
+        
+    case 4  : 
+        print("Wednesday")       
+
+    case 5:
+        print("Thursday")    
+        
+    case 6:
+        print("Friday")    
+
+    case 7:
+        print("Saturday")    
+     
+    case _:
+        print("Invalid Day")    
+        
+        
+        
+        
+        
+#LOOP IN PYTHON
+
+        
+name="Himanshu"
+
+for i in name:
+    print(i, end=", ")
+    
+    
+    colors=["Red", "Blue","Green","Yellow"]  
+
+for z in colors:
+    print(z)
+    
+    
+    
+    
+for k in range (200000000):
+    print(k+1)
+        
+        
+        
+        
+        
+        
+        
+           
+                 
+        
