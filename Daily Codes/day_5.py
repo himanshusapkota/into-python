abe4ac683eb38305e2e0cbdb000baf9f8a0071d0
himanshusapkota <himanshuspkt@gmail.com>
2026-08-03 13:00:34 +0545

    HAHA

---
+++ Daily Codes/day_5.py
@@ -0,0 +1,33 @@
+#While Loop
+#Example Code
+
+i=1
+while(i<=5):
+    print(i)
+    i=i+1
+    
+    #OUTPUT 
+       #1
+       #2
+       #3
+       #4
+       #5
+j=0
+while j<56:
+    j=int(input("Enter The Number dwag: "))       
+    print(j)
+       
+       
+print("Done With The Loop")       
+
+
+#Break and Continue Statement
+
+k=0
+
+while True:
+    k=int(input("Enter The Number : "))
+
+
+    if(k==6):
+        break
