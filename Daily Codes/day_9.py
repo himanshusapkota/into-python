613531ab8aa552bd41867451de8420223a17a357
himanshusapkota <himanshuspkt@gmail.com>
2026-08-09 10:21:16 +0545

    Commit

---
+++ Daily Codes/day_9.py
@@ -0,0 +1,15 @@
+
+#OLD STYLE
+
+letter="Hey My Name is {1} and I am from {0}"
+
+name="Himanshu"
+country="Nepal"
+
+print(letter.format(country,name))
+
+
+
+#F-STRING
+
+print(f"Heyy My name is {name} and I am from {country}")
