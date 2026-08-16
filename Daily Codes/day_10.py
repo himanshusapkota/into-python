764203dee693ca38ff0038b1006dc5eecb57dc6b
himanshusapkota <himanshuspkt@gmail.com>
2026-08-16 13:52:25 +0545

    BackLog

---
+++ Daily Codes/day_10.py
@@ -0,0 +1,14 @@
+#DOC STRING:
+
+def square(n):
+  '''Takes The Number N and Return The Square of N''' #we have to write docstring right below the function AHHAAHA
+  print(n**2)
+  
+
+square(8)   
+print(square.__doc__)
+
+#DOCSTRING IS NOT IGNORED AND IT IS VERY HELPFUL
+#to print the docstring we have to use 
+#print(function_name.__doc__)
+
