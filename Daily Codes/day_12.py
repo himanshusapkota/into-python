9ad05f356569ef9148c8eb7be478c0c8d891c4ab
himanshusapkota <himanshuspkt@gmail.com>
2026-08-19 13:03:34 +0545

    PY

--- Daily Codes/day_12.py
+++ Daily Codes/day_12.py
@@ -1,1 +1,27 @@
 #RECURSION
+#Factorial
+
+def factorial(n):
+    if (n==0 or n==1):
+        return 1
+    
+    else:
+        return n*factorial(n-1)
+    
+print(factorial(3))    
+
+
+
+#fibonacci 
+
+def fibonacci(n):
+  if n <= 1:
+    return n
+  else:
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+print(fibonacci(7))
+
+
+
+        
---
+++ Daily Codes/day_13.py
@@ -0,0 +1,7 @@
+#SET __ It doesnot take the duplicate Value i.e you cant add the duplicate value
+
+a={1,2,3,1}
+print(a)
+
+b={"Himanshu",16, True}
+print(b)
